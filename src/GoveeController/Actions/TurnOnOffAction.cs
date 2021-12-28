c4b3b14027aa9a813f91fc8121669ce00360770a
Richard Herman <geekyeggo@users.noreply.github.com>
2021-12-28 21:41:51 +0000

    Initial action framework

---
+++ src/GoveeController/Actions/TurnOnOffAction.cs
@@ -0,0 +1,17 @@
+﻿namespace GoveeController.Actions
+{
+    using System.Threading.Tasks;
+    using SharpDeck;
+    using SharpDeck.Events.Received;
+
+    /// <summary>
+    /// Provides an action that is capable of controlling the on/off state of a device.
+    /// </summary>
+    [StreamDeckAction("com.geekyeggo.goveecontroller.turnonoff")]
+    public class TurnOnOffAction : StreamDeckAction
+    {
+        /// <inheritdoc/>
+        protected override Task OnKeyDown(ActionEventArgs<KeyPayload> args)
+            => base.OnKeyDown(args);
+    }
+}
---
+++ src/GoveeController/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace GoveeController
+{
+    using GoveeController.GoveeApi;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using SharpDeck.Extensions.Hosting;
+
+    /// <summary>
+    /// The plugin.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+#if DEBUG
+            System.Diagnostics.Debugger.Launch();
+#endif
+
+            new HostBuilder()
+                .ConfigureServices(services => services.AddSingleton<GoveeHttpClient>())
+                .UseStreamDeck()
+                .Start();
+        }
+    }
+}
