1b2e55d1f9f96477a2ca0e5740ec06d49327fcca
Richard Herman <geekyeggo@users.noreply.github.com>
2022-01-03 01:11:46 +0000

    Added set color action

---
+++ src/GoveeController/Actions/ColorAction.cs
@@ -0,0 +1,71 @@
+﻿namespace GoveeController.Actions
+{
+    using System.Diagnostics.CodeAnalysis;
+    using System.Drawing;
+    using System.Threading.Tasks;
+    using GoveeController.Actions.Settings;
+    using GoveeController.Govee.Models;
+    using GoveeController.Services;
+    using SharpDeck;
+    using SharpDeck.Events.Received;
+
+    /// <summary>
+    /// Provides an action capable of setting the color of a device.
+    /// </summary>
+    [StreamDeckAction("com.geekyeggo.goveecontroller.color")]
+    public class ColorAction : ActionBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorAction"/> class.
+        /// </summary>
+        /// <param name="goveeService">The Govee service.</param>
+        public ColorAction(IGoveeService goveeService)
+            : base(goveeService, CommandType.Color)
+        {
+        }
+
+        /// <inheritdoc/>
+        protected override async Task OnKeyDown(ActionEventArgs<KeyPayload> args)
+        {
+            var settings = args.Payload.GetSettings<ColorSettings>();
+            var device = await this.GoveeService.GetDeviceInfoAsync(settings.DeviceId);
+
+            if (TryParseColor(settings.Color, out var color))
+            {
+                await this.GoveeService.Client.SetColorAsync(device.Device, device.Model, color.R, color.G, color.B);
+                await this.ShowOkAsync();
+            }
+            else
+            {
+                await this.ShowAlertAsync();
+            }
+        }
+
+        /// <summary>
+        /// Tries to parse the color from the specified <paramref name="hex"/>.
+        /// </summary>
+        /// <param name="hex">The hexadecimal value that represents the color.</param>
+        /// <param name="value">The result of parsing the hexadecimal value.</param>
+        /// <returns><c>true</c> when the color was parsed; otherwise <c>false</c>.</returns>
+        private static bool TryParseColor(string? hex, [NotNullWhen(true)] out Color value)
+        {
+            if (string.IsNullOrWhiteSpace(hex))
+            {
+                value = Color.White;
+                return false;
+            }
+
+            try
+            {
+                value = ColorTranslator.FromHtml(hex);
+                return true;
+            }
+            catch
+            {
+                value = Color.White;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/GoveeController/Actions/Settings/ColorSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace GoveeController.Actions.Settings
+{
+    public class ColorSettings
+    {
+        /// <summary>
+        /// Gets or sets the device identifier.
+        /// </summary>
+        public string? DeviceId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color.
+        /// </summary>
+        public string? Color { get; set; }
+    }
+}
