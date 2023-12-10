f4b7ea99c8ab76f74f9e05121cffd916c0b6a76f
Richard Herman <geekyeggo@users.noreply.github.com>
2023-12-10 17:18:34 +0000

    refactor: basic structure

---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import path from "node:path";
+import url from "node:url";
+
+const isWatching = !!process.env.ROLLUP_WATCH;
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = {
+	input: "src/plugin.ts",
+	output: {
+		file: "com.geekyeggo.goveecontroller.sdPlugin/bin/plugin.js",
+		sourcemap: isWatching,
+		sourcemapPathTransform: (relativeSourcePath, sourcemapPath) => {
+			return url.pathToFileURL(path.resolve(path.dirname(sourcemapPath), relativeSourcePath)).href;
+		}
+	},
+	plugins: [
+		typescript({
+			mapRoot: isWatching ? "./" : undefined
+		}),
+		nodeResolve({
+			browser: false,
+			exportConditions: ["node"],
+			preferBuiltins: true
+		}),
+		commonjs(),
+		!isWatching && terser(),
+		{
+			name: "emit-module-package-file",
+			generateBundle() {
+				this.emitFile({ fileName: "package.json", source: `{ "type": "module" }`, type: "asset" });
+			}
+		}
+	]
+};
+
+export default config;
